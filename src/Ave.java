6822ce07f11d36a067becbf52a571972052c2929
Luis Villas <shusten@hotmail.com>
2019-12-12 16:54:55 -0300

    Add Ave

---
+++ src/Ave.java
@@ -0,0 +1,25 @@
+public class Ave extends Animal {
+    private String corPena;
+
+    @Override
+    public void locomover() {
+        System.out.println("Voando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo Frutas");
+
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de Ave");
+    }
+    public void fazerNinho() {
+        System.out.println("Fazendo ninho");
+
+    }
+
+
+}
